6a2a24248b096097a4a6132f86748e58c7dc148b
maciup7 <marekkswi@gmail.com>
2023-06-13 21:50:07 +0200

    Zadanie 1 pierwsza wersja

--- zadanie1/zadanie1.php
+++ zadanie1/zadanie1.php
@@ -1,14 +1,47 @@
+<!DOCTYPE html>
 <?php
-    class abc{
-        public function make() {
 
+    class Wyliczenie{
+        public $liczba;
+
+        public function podajliczbe($x){
+            $this->liczba= $x;
         }
+
+        public function make($xx){
+            for ($i = 1; $i <= 3; $i++) {
+                switch ($i){
+                    case 1:
+                        $x=$xx*3;
+                        echo $x;
+                        break;
+                    case 2:
+                        $x=$x+1;
+                        echo $x;
+                        break;
+                    case 3:
+                        $x=$x/2;
+                        echo $x;
+                        break;
+                }
+            }
+            
+        }
+
     }
+
+
 ?>
 <html>
 <head>
 </head>
 <body>
     <a href="../index.php"> Home</a>
+    <?php
+        $xx=3;
+        $Wyliczenie1= new Wyliczenie;
+        $Wyliczenie1->make($xx);
+
+    ?>
 </body>
 </html>
