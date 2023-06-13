108aa53dfe9950b696b3830e2465a5e0920dfc34
maciup7 <marekkswi@gmail.com>
2023-06-13 16:45:13 +0200

    Pierwszy commit, test działania gita

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+</head>
+<body>
+    <a href="zadanie1/zadanie1.php">zadanie 1</a>
+    <a href="zadanie2/zadanie2.php">zadanie 2</a>
+    <a href="zadanie3/zadanie3.php">zadanie 3</a>
+    <a href="zadanie4/zadanie4.php">zadanie 4</a>
+
+</body>
+</html>
---
+++ zadanie1/zadanie1.php
@@ -0,0 +1,14 @@
+<?php
+    class abc{
+        public function make() {
+
+        }
+    }
+?>
+<html>
+<head>
+</head>
+<body>
+    <a href="../index.php"> Home</a>
+</body>
+</html>
